b6ca0db871b186084a99bbfec2be33fcdc3acc21
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-05-29 06:41:33 +0300

    Added the photoes in the project, and added also the functions for thier transition

--- lib/choose_animal.dart
+++ lib/choose_animal.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
+final randomizer = Random();
+
+int getRandom(int min, int max) {
+  return min + randomizer.nextInt(max - min);
+}
+
 class ChooseAnimal extends StatefulWidget {
   const ChooseAnimal({super.key});
 
@@ -10,16 +18,80 @@ class ChooseAnimal extends StatefulWidget {
 }
 
 class _ChooseAnimalState extends State<ChooseAnimal> {
-  void pickACat() {}
+  var currImage = '../assets/animals_images/bird1.jpg';
+
+  List<String> allBirds = [
+    '../assets/animals_images/bird1.jpg',
+    '../assets/animals_images/bird2.jpg',
+    '../assets/animals_images/bird3.jpg',
+    '../assets/animals_images/bird4.jpg',
+    '../assets/animals_images/bird5.jpg',
+    '../assets/animals_images/bird6.jpg',
+  ];
+
+  List<String> allCamels = [
+    '../assets/animals_images/camel1.jpg',
+    '../assets/animals_images/camel2.jpg',
+    '../assets/animals_images/camel3.jpg',
+    '../assets/animals_images/camel4.jpg',
+  ];
+
+  List<String> allCats = [
+    '../assets/animals_images/cat1.jpg',
+    '../assets/animals_images/cat2.jpg',
+    '../assets/animals_images/cat3.jpg',
+    '../assets/animals_images/cat4.jpg',
+    '../assets/animals_images/cat5.jpg',
+  ];
+
+  List<String> otherAnimals = [
+    '../assets/animals_images/bear1.jpg',
+    '../assets/animals_images/deer1.jpg',
+    '../assets/animals_images/dolphin1.jpg',
+    '../assets/animals_images/lion1.jpg',
+    '../assets/animals_images/tiger1.jpg',
+  ];
+
+  void changeImage(List<String> arr) {
+    setState(() {
+      currImage = arr[getRandom(0, arr.length - 1)];
+    });
+  }
+
+  void pickACat() {
+    changeImage(allCats);
+  }
+
+  void pickACamel() {
+    changeImage(allCamels);
+  }
+
+  void pickABird() {
+    changeImage(allBirds);
+  }
+
+  void pickAnAnimal() {
+    changeImage(otherAnimals);
+  }
 
   @override
   Widget build(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        Image.asset(
+          currImage,
+          width: 350,
+          height: 350,
+        ),
+        const SizedBox(
+          height: 25,
+          width: 25,
+        ),
         Row(
           mainAxisSize: MainAxisSize.min,
           children: [
+            // Cat button
             TextButton(
               style: TextButton.styleFrom(
                 shadowColor: Colors.black,
@@ -33,14 +105,15 @@ class _ChooseAnimalState extends State<ChooseAnimal> {
               height: 25,
               width: 25,
             ),
+            // Bird button
             TextButton(
               style: TextButton.styleFrom(
                 shadowColor: Colors.black,
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(fontSize: 28),
               ),
-              onPressed: pickACat,
-              child: const Text('Show a cat'),
+              onPressed: pickABird,
+              child: const Text('Show a bird'),
             ),
           ],
         ),
@@ -51,26 +124,28 @@ class _ChooseAnimalState extends State<ChooseAnimal> {
         Row(
           mainAxisSize: MainAxisSize.min,
           children: [
+            // Camel button
             TextButton(
               style: TextButton.styleFrom(
                 shadowColor: Colors.black,
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(fontSize: 28),
               ),
-              onPressed: pickACat,
-              child: const Text('Show a cat'),
+              onPressed: pickACamel,
+              child: const Text('Show a Camel'),
             ),
             const SizedBox(
               height: 25,
               width: 25,
             ),
+            // others button
             TextButton(
               style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(fontSize: 28),
               ),
-              onPressed: pickACat,
-              child: const Text('Show a cat'),
+              onPressed: pickAnAnimal,
+              child: const Text('Show other animals'),
             ),
           ],
         ),
