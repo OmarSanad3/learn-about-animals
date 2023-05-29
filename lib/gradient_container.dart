8a156b424469bd9a2676f56815c095d5bd0e1ea6
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-05-29 03:56:54 +0300

    Added the gradient background

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(
+      {super.key,
+      this.gradientColors = const [
+        Color.fromRGBO(187, 222, 251, 1),
+        Color.fromRGBO(21, 101, 192, 1)
+      ],
+      this.beginOfGradient = Alignment.topLeft,
+      this.endOfGradient = Alignment.topLeft});
+
+  final List<Color> gradientColors;
+  final Alignment beginOfGradient;
+  final Alignment endOfGradient;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: gradientColors,
+        begin: beginOfGradient,
+        end: endOfGradient,
+      )),
+    );
+  }
+}
