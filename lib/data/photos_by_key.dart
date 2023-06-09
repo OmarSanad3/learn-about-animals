204cd8735ceb2e013835ab276773204fb9bbd403
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 00:28:13 +0300

    Added a file where we map a name of an animal with the files for this animal

---
+++ lib/data/photos_by_key.dart
@@ -0,0 +1,23 @@
+Map<String, List<String>> photosMap = {
+  'bird': [
+    'assets/animals_images/bird1.jpg',
+    'assets/animals_images/bird2.jpg',
+    'assets/animals_images/bird3.jpg',
+    'assets/animals_images/bird4.jpg',
+    'assets/animals_images/bird5.jpg',
+    'assets/animals_images/bird6.jpg',
+  ],
+  'camel': [
+    'assets/animals_images/camel1.jpg',
+    'assets/animals_images/camel2.jpg',
+    'assets/animals_images/camel3.jpg',
+    'assets/animals_images/camel4.jpg',
+  ],
+  'cat': [
+    'assets/animals_images/cat1.jpg',
+    'assets/animals_images/cat2.jpg',
+    'assets/animals_images/cat3.jpg',
+    'assets/animals_images/cat4.jpg',
+    'assets/animals_images/cat5.jpg',
+  ],
+};
