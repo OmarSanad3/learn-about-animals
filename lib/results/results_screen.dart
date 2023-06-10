f96f7c0832be48548717084b10cb2d87b63464ae
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 19:43:14 +0300

    Added getSummaryData function

--- lib/results/results_screen.dart
+++ lib/results/results_screen.dart
@@ -1,9 +1,11 @@
+import 'package:animals_photos/modules/photos_paths.dart';
 import 'package:animals_photos/quiz/quiz_questions.dart';
+import 'package:animals_photos/results/results_block.dart';
 import 'package:animals_photos/start_button.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({
+  ResultsScreen({
     super.key,
     required this.answers,
     required this.quizQuestions,
@@ -11,11 +13,26 @@ class ResultsScreen extends StatelessWidget {
   });
 
   final List<String> answers;
-  final List<QuizQuestions> quizQuestions;
+  final List<PhotoPath> quizQuestions;
   final void Function() backToHomeScreen;
+
+  List<Map<String, Object>> getSummaryData() {
+    List<Map<String, Object>> theSummary = [];
+    for (int i = 0; i < answers.length; i++) {
+      theSummary.add({
+        'index': i,
+        'photo': quizQuestions[i].path,
+        'correct-answer': quizQuestions[i].name,
+        'user-answer': answers[i],
+      });
+    }
+    return theSummary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, Object>> summaryData = getSummaryData();
+
     return Column(
       children: [
         const Text(
@@ -25,7 +42,7 @@ class ResultsScreen extends StatelessWidget {
           margin: const EdgeInsets.all(5),
           height: 500,
           child: SingleChildScrollView(
-            child: ResultsBlock(),
+            child: ResultsBlock(summary: summaryData),
           ),
         ),
         StartButton(
