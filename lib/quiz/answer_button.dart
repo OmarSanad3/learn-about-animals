e7aed7850432ebad7be8e9d08f8a4a3899d2ae32
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-11 01:39:21 +0300

    Added the capitalizeFirstLetter function

--- lib/quiz/answer_button.dart
+++ lib/quiz/answer_button.dart
@@ -6,13 +6,17 @@ class AnswerButton extends StatelessWidget {
   final String text;
   final void Function(String chosenAnswer) onTab;
 
+  String capitalizeFirstLetter(String s) {
+    return '${s[0].toUpperCase()}${s.substring(1).toLowerCase()}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
         onTab(text);
       },
-      child: Text(text),
+      child: Text(capitalizeFirstLetter(text)),
     );
   }
 }
