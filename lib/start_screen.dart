608c99a49fad2f7226c01838ed07121b2daa57ff
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 18:53:35 +0300

    Added photo and two buttons to the startscreen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,22 +2,37 @@ import 'package:animals_photos/start_button.dart';
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen({
+    super.key,
+    required this.onTabQuiz,
+    required this.onTabStudy,
+  });
+
+  final void Function() onTabStudy, onTabQuiz;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        StartButton(
-          buttonText: 'Study!',
-          onTab: () {},
-          buttonIcon: const Icon(Icons.menu_book_sharp),
+        Image.asset(
+          'assets/animals_images/animals_start_screen.jpg',
         ),
-        StartButton(
-          buttonText: 'Start Quiz!',
-          onTab: () {},
-          buttonIcon: const Icon(Icons.keyboard_arrow_right),
+        const SizedBox(height: 100),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            StartButton(
+              buttonText: 'Study!',
+              onTab: onTabStudy,
+              buttonIcon: const Icon(Icons.menu_book_sharp),
+            ),
+            StartButton(
+              buttonText: 'Start Quiz!',
+              onTab: onTabQuiz,
+              buttonIcon: const Icon(Icons.keyboard_arrow_right),
+            ),
+          ],
         ),
       ],
     );
