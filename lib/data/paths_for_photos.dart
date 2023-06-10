628805967223730d680dc51db93f755525cbc58d
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-11 02:30:20 +0300

    Updated the assets

--- lib/data/paths_for_photos.dart
+++ lib/data/paths_for_photos.dart
@@ -19,5 +19,36 @@ List<PhotoPath> pathsPhotos = [
   const PhotoPath(name: 'cat', path: 'assets/animals_images/cat4.jpg'),
   const PhotoPath(name: 'cat', path: 'assets/animals_images/cat5.jpg'),
 //
-  // const PhotoPath(name: 'bird', path: '/assets/animals_images/cat5.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse1.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse2.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse3.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse4.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse5.jpg'),
+  const PhotoPath(name: 'horse', path: 'assets/animals_images/horse6.jpg'),
+//
+  const PhotoPath(name: 'donkey', path: 'assets/animals_images/donkey1.jpg'),
+  const PhotoPath(name: 'donkey', path: 'assets/animals_images/donkey2.jpg'),
+  const PhotoPath(name: 'donkey', path: 'assets/animals_images/donkey3.jpg'),
+  const PhotoPath(name: 'donkey', path: 'assets/animals_images/donkey4.jpg'),
+//
+  const PhotoPath(name: 'lion', path: 'assets/animals_images/lion1.jpg'),
+  const PhotoPath(name: 'lion', path: 'assets/animals_images/lion2.jpg'),
+  const PhotoPath(name: 'lion', path: 'assets/animals_images/lion3.jpg'),
+  const PhotoPath(name: 'lion', path: 'assets/animals_images/lion4.jpg'),
+  const PhotoPath(name: 'lion', path: 'assets/animals_images/lion5.jpg'),
+//
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger1.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger2.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger3.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger4.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger5.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger6.jpg'),
+  const PhotoPath(name: 'tiger', path: 'assets/animals_images/tiger7.jpg'),
+//
+  const PhotoPath(name: 'cheeta', path: 'assets/animals_images/cheeta1.jpg'),
+  const PhotoPath(name: 'cheeta', path: 'assets/animals_images/cheeta2.jpg'),
+  const PhotoPath(name: 'cheeta', path: 'assets/animals_images/cheeta3.jpg'),
+  const PhotoPath(name: 'cheeta', path: 'assets/animals_images/cheeta4.jpg'),
+  const PhotoPath(name: 'cheeta', path: 'assets/animals_images/cheeta5.jpg'),
+//
 ];
