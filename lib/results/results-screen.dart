b0a087792f754cd8fa91f278b8ae83503fd5a290
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 19:29:27 +0300

    Added a button in the results screen to direct us to the home screen

--- lib/results/results-screen.dart
+++ lib/results/results-screen.dart
@@ -1,3 +1,4 @@
+import 'package:animals_photos/start_button.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
@@ -11,8 +12,15 @@ class ResultsScreen extends StatelessWidget {
         Container(
           margin: const EdgeInsets.all(5),
           height: 500,
-          child : SingleChildScrollView(child: ,),
+          child: SingleChildScrollView(
+            child: ResultsBlock(),
+          ),
         ),
+        StartButton(
+          buttonText: 'Home Screen',
+          onTab: () {},
+          buttonIcon: const Icon(Icons.home_filled),
+        )
       ],
     );
   }
