1b5ef7da7b6d6747d74b3cbc8a277938d99a2352
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 18:57:40 +0300

    Made a lot of configurations to the file

--- lib/animals_project.dart
+++ lib/animals_project.dart
@@ -1,6 +1,13 @@
-import 'package:animals_photos/start_button.dart';
+import 'package:animals_photos/quiz/choose_num_questions.dart';
+import 'package:animals_photos/quiz_questions.dart';
 import 'package:animals_photos/start_screen.dart';
+import 'package:animals_photos/study/study_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'data/paths_for_photos.dart';
+
+final randomizer = Random();
 
 class AnimalsProject extends StatefulWidget {
   const AnimalsProject({
@@ -8,8 +15,8 @@ class AnimalsProject extends StatefulWidget {
     this.beginOfGradient = Alignment.topLeft,
     this.endOfGradient = Alignment.topLeft,
     this.gradientColors = const [
-      Color.fromRGBO(187, 222, 251, 1),
-      Color.fromRGBO(21, 101, 192, 1)
+      Color.fromARGB(255, 128, 174, 49),
+      Color.fromARGB(255, 70, 102, 29),
     ],
   });
 
@@ -24,8 +31,17 @@ class AnimalsProject extends StatefulWidget {
 }
 
 class _AnimalsProjectState extends State<AnimalsProject> {
+  String activeScreen = 'start-screen';
+  Widget? currScreen;
+
   Alignment? beginOfGradient, endOfGradient;
   List<Color>? gradientColors;
+
+  List<String> chosenAnswer = [];
+
+  List<int> displayedQuestions = [];
+
+  int numOfQuestion = 0, questionIdx = 0;
 
   @override
   void initState() {
@@ -35,8 +51,90 @@ class _AnimalsProjectState extends State<AnimalsProject> {
     super.initState();
   }
 
+  int getRandom(int min, int max) {
+    return min + randomizer.nextInt(max - min);
+  }
+
+  List<int> generateQuestions(int numQuestions) {
+    int idx;
+    List<int> indexesQuestions = [];
+    Set<int> takenIndexes = {};
+    while (indexesQuestions.length < numQuestions) {
+      idx = getRandom(0, pathsPhotos.length - 1);
+      if (!takenIndexes.contains(idx)) {
+        takenIndexes.add(idx);
+        indexesQuestions.add(idx);
+      }
+    }
+
+    return indexesQuestions;
+  }
+
+  void goStudy() {
+    setState(() {
+      activeScreen = 'study-screen';
+    });
+  }
+
+  void takeQuiz() {
+    setState(() {
+      activeScreen = 'quiz-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    setState(() {
+      chosenAnswer.add(answer);
+      if (chosenAnswer.length == displayedQuestions.length) {
+        activeScreen = 'results-screen';
+        chosenAnswer.clear();
+      } else {
+        questionIdx++;
+      }
+    });
+  }
+
+  void getNumQuestion(int numQ) {
+    setState(() {
+      numOfQuestion = numQ;
+      activeScreen = 'question-screen';
+      displayedQuestions = generateQuestions(numQ);
+    });
+  }
+
+  void goStartScreen() {
+    setState(() {
+      activeScreen = 'start-screen';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (activeScreen == 'start-screen') {
+      currScreen = StartScreen(
+        onTabQuiz: takeQuiz,
+        onTabStudy: goStudy,
+      );
+    }
+
+    if (activeScreen == 'study-screen') {
+      currScreen = StudyScreen(backToStartScreen: goStartScreen);
+    }
+
+    if (activeScreen == 'quiz-screen') {
+      currScreen = ChooseNumQuestions(
+        backToStartScreen: goStartScreen,
+        getNumOfQuestions: getNumQuestion,
+      );
+    }
+
+    if (activeScreen == 'question-screen') {
+      currScreen = QuizQuestions(
+        questionIdx: questionIdx,
+        chooseAns: chooseAnswer,
+      );
+    }
+
     return Container(
       height: double.infinity,
       width: double.infinity,
@@ -47,7 +145,7 @@ class _AnimalsProjectState extends State<AnimalsProject> {
           end: endOfGradient as Alignment,
         ),
       ),
-      child: const StartScreen(),
+      child: currScreen,
     );
   }
 }
