f86e70ccc3fd06e4274276d84e34cbd1df0900af
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 17:22:17 +0300

    Added a generate questions function

--- lib/quiz/display_question.dart
+++ lib/quiz/display_question.dart
@@ -15,9 +15,35 @@ class DisplayQuestion extends StatelessWidget {
     return min + randomizer.nextInt(max - min);
   }
 
+  List<String> generateChoises() {
+    List<String> choises = [pathsPhotos[questionIdx].name];
+    Set<String> takenIdexes = {pathsPhotos[questionIdx].name};
+    int currIdx;
+    while (choises.length < 4) {
+      currIdx = getRandom(0, allAnimalsNames.length - 1);
+      if (!takenIdexes.contains(allAnimalsNames[currIdx])) {
+        takenIdexes.add(allAnimalsNames[currIdx]);
+        choises.add(allAnimalsNames[currIdx]);
+      }
+    }
+
+    // change the place of the first choise
+    currIdx = getRandom(0, choises.length - 1);
+    String s1 = choises[0];
+    String s2 = choises[currIdx];
+    String tmp = s1;
+    s1 = s2;
+    s2 = tmp;
+
+    choises[0] = s1;
+    choises[currIdx] = s2;
+
+    return choises;
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<String> choises = [];
+    List<String> choises = generateChoises();
 
     while (choises.length < 4) {}
 
