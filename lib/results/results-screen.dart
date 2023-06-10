aeb26083502f41e35424155db569e440cb36737f
OmarSanad3 <omarmohamedsanad3@gmail.com>
2023-06-10 19:27:42 +0300

    Added a scrollable container witch will display the results

--- lib/results/results-screen.dart
+++ lib/results/results-screen.dart
@@ -8,6 +8,11 @@ class ResultsScreen extends StatelessWidget {
         const Text(
           'You have Answered (x) correct questions out of (y) questions',
         ),
+        Container(
+          margin: const EdgeInsets.all(5),
+          height: 500,
+          child : SingleChildScrollView(child: ,),
+        ),
       ],
     );
   }
